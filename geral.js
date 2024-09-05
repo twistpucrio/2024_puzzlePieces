6094fbc4446e30299eaf03cf4d557771c4a7e584
ju-s11 <julia.g.s.jso@gmail.com>
2024-09-05 17:13:33 -0300

    commit antes da dani assumir

---
+++ geral.js
@@ -0,0 +1,107 @@
+
+var currTile;
+var otherTile;
+
+var turns = 0;
+
+var imgOrder;
+
+function game(rows, columns) {
+
+    imgOrder = create_imgOrder(rows, columns);
+
+    for (let r=0; r < rows; r++) {
+        for (let c=0; c < columns; c++) {
+
+            //<img id="0-0" src="1. ou webp">
+            let tile = document.createElement("img");
+            tile.id = r.toString() + c.toString();
+            tile.src = imgOrder.shift() + ".webp";
+
+            //DRAG FUNCTIONALITY
+            tile.addEventListener("dragstart", dragStart);  //click an image to drag
+            tile.addEventListener("dragover", dragOver);    //moving image around while clicked
+            tile.addEventListener("dragenter", dragEnter);  //dragging image onto another one
+            tile.addEventListener("dragleave", dragLeave);  //dragged image leaving anohter image
+            tile.addEventListener("drop", dragDrop);        //drag an image over another image, drop the image
+            tile.addEventListener("dragend", dragEnd);      //after drag drop, swap the two tiles
+
+            document.getElementById("board").append(tile);
+        }
+    }
+}
+
+function create_imgOrder(rows, columns) {
+    let imgOrder = [];
+    for (let r=0; r < rows; r++) {
+        for (let c=0; c < columns; c++) {
+            imgOrder.push(r.toString() + c.toString());
+        }
+    }
+    return imgOrder;
+}
+
+function play_audio() {
+    let x = document.getElementById("audio");
+    x.play();
+}
+
+function pause_audio() {
+    let x = document.getElementById("audio");
+    x.pause();
+}
+
+function dragStart() {
+    currTile = this; //this refers to the img tile being dragged
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+    otherTile = this; //this refers to the img tile being dropped on
+}
+
+function dragEnd() {
+    if (!otherTile.src.includes("02.webp")) { //.jpg ou .webp
+        return;
+    }
+
+    let currCoords = currTile.id.split(""); //ex) "0-0" -> ["0", "0"]
+    let r = parseInt(currCoords[0]);
+    let c = parseInt(currCoords[1]);
+
+    let otherCoords = otherTile.id.split("");
+    let r2 = parseInt(otherCoords[0]);
+    let c2 = parseInt(otherCoords[1]);
+
+    let moveLeft = r == r2 && c2 == c-1;
+    let moveRight = r == r2 && c2 == c+1;
+
+    let moveUp = c == c2 && r2 == r-1;
+    let moveDown = c == c2 && r2 == r+1;
+
+    let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
+
+    if (isAdjacent) {
+        let currImg = currTile.src;
+        let otherImg = otherTile.src;
+
+        currTile.src = otherImg;
+        otherTile.src = currImg;
+
+        turns += 1;
+        document.getElementById("turns").innerText = turns;
+    }
+
+
+}
---
+++ nivel-1/nivel-1.js
@@ -0,0 +1,4 @@
+
+window.onload = function() {
+    game(3, 3); 
+}
