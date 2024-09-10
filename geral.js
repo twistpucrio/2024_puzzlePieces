ffe51d3108120f95c11fe83bc3818db8c4a234e4
Danielle Guimarães <danielleguimaraes@tecgraf.puc-rio.br>
2024-09-10 17:35:31 -0300

    Adicionando o JavaScript da página da rapunzel com o html pronto e css semi pronto.

--- geral.js
+++ geral.js
@@ -1,103 +1,123 @@
+
+
+var rows;
+var columns;
+
 var currTile;
 var otherTile;
 
 var turns = 0;
-var imgOrder = []; 
-var imgOrderCopy = [];
+var imgOrder;
+var rightOrder;
+var lista; //Copia do imgOrder
 
-function game(rows, columns) {
-    imgOrder = create_imgOrder(rows, columns);
-
-    shuffle(imgOrder);
-
-    imgOrderCopy = [...imgOrder];
-
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < columns; c++) {
-            let tile = document.createElement("img");
-            tile.id = r.toString() + c.toString();
-            tile.src = "img/" + imgOrder.shift() + ".jpg"; 
-            console.log(tile.src);
-
-            tile.addEventListener("dragstart", dragStart);
-            tile.addEventListener("dragover", dragOver);
-            tile.addEventListener("dragenter", dragEnter);
-            tile.addEventListener("dragleave", dragLeave);
-            tile.addEventListener("drop", dragDrop);
-            tile.addEventListener("dragend", dragEnd);
-
-            document.getElementById("board").append(tile);
-        }
-    }
-}
-
-function shuffle(array) {
+function shuffle(array){
     let currentIndex = array.length;
     
-    while (currentIndex !== 0) {
-
+    while (currentIndex != 0) {
+        
         let randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
         
-        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
     }
+}
+        
+        
+function arraysIguais() {
+    if (lista.length !== rightOrder.length) {
+        return false;
+    }
+    
+    for (let i = 0; i < rightOrder.length; i++) {
+        if (lista[i] !== rightOrder[i]) {
+            return false; 
+        }
+    }
+    
+    return true;
+}
+
+function resolvido(){
+   
+    if(arraysIguais()){
+        alert("resolveu... :(")
+    }
+    
 }
 
 function create_imgOrder(rows, columns) {
     let order = []; 
+
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < columns; c++) {
             order.push(r.toString() + c.toString());
         }
     }
+
     return order; 
+
 }
 
-function atualizaLista(img1Id,index1,img2Id,index2){
-    if(index1 !=-1){
-        imgOrderCopy[index1] = img2Id;
+function searchAndReplace(Item, index,newItem,index2){
+    
+    if (index !== -1) {
+        lista[index] = newItem;
     }
-    if(index2 !=-1){
-        imgOrderCopy[index2] = img1Id;
-    }
+
+    if (index !== -1) {
+        lista[index2] = Item;
+    }   
+    
 }
 
-function compareLists( ) {
-    if (imgOrder.length !== imgOrderCopy.length) {
-        return false;
-    }
+function game(rows,columns) {
+    imgOrder = create_imgOrder(rows,columns);
 
-    for (let i = 0; i < imgOrderCopy.length; i++) {
-        if (imgOrderCopy[i] !== imgOrder[i]) {
-            return false;
+    rightOrder = [...imgOrder];
+
+    shuffle(imgOrder);
+
+    lista = [...imgOrder];
+    
+    for (let r=0; r < rows; r++) {
+        for (let c=0; c < columns; c++) {
+            
+            var tile = document.createElement("img");
+            tile.id = r.toString() + c.toString();
+
+
+            tile.src = "img/" + imgOrder.shift() + ".jpg";
+
+            //DRAG FUNCTIONALITY
+            tile.addEventListener("dragstart", dragStart);  //click an image to drag
+            tile.addEventListener("dragover", dragOver);    //moving image around while clicked
+            tile.addEventListener("dragenter", dragEnter);  //dragging image onto another one
+            tile.addEventListener("dragleave", dragLeave);  //dragged image leaving anohter image
+            tile.addEventListener("drop", dragDrop);        //drag an image over another image, drop the image
+            tile.addEventListener("dragend", dragEnd);      //after drag drop, swap the two tiles
+            document.getElementById("board").append(tile);
         }
     }
 
-    return true;
-}
-
-function resolvido(){
-    if(ehIgual()){
-        alert("resolvido :(");
-    }
 }
 
 function play_audio() {
     let x = document.getElementById("audio");
     x.play();
 }
-
 function pause_audio() {
     let x = document.getElementById("audio");
     x.pause();
 }
 
 function dragStart() {
-    currTile = this; 
+    currTile = this; //this refers to the img tile being dragged
 }
 
 function dragOver(e) {
-    e.preventDefault(); 
+    e.preventDefault();
 }
 
 function dragEnter(e) {
@@ -105,48 +125,63 @@ function dragEnter(e) {
 }
 
 function dragLeave() {
+    
 }
 
 function dragDrop() {
-    otherTile = this; 
+    otherTile = this; //this refers to the img tile being dropped on
 }
-function dragEnd() {
-    if (!otherTile.src.includes("02.jpg")) { 
-    }
 
-    let currCoords = currTile.id.split(""); 
+function dragEnd() {
+    
+
+    if (!otherTile.src.includes("02.jpg")) { //.jpg ou .webp
+        return;
+    }
+    
+    let currCoords = currTile.id.split(""); //ex) "0-0" -> ["0", "0"]
     let r = parseInt(currCoords[0]);
     let c = parseInt(currCoords[1]);
-
+    
     let otherCoords = otherTile.id.split("");
     let r2 = parseInt(otherCoords[0]);
     let c2 = parseInt(otherCoords[1]);
-
+    
+    
+    
     let moveLeft = r == r2 && c2 == c-1;
     let moveRight = r == r2 && c2 == c+1;
+    
     let moveUp = c == c2 && r2 == r-1;
     let moveDown = c == c2 && r2 == r+1;
-
+    
     let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
-
+    
     if (isAdjacent) {
         let currImg = currTile.src;
         let otherImg = otherTile.src;
-
+        
         currTile.src = otherImg;
         otherTile.src = currImg;
-
+        
         turns += 1;
         document.getElementById("turns").innerText = turns;
     }
+    
+    let r1String=r.toString();
+    let c1String=c.toString();
+    let r2String=r2.toString();
+    let c2String=c2.toString();
 
-    let img1Id = r.toString() + c.toString();
-    let img2Id = r2.toString() + c2.toString();
-    
-    let index1 = imgOrderCopy.indexOf(img1Id);
-    let index2 = imgOrderCopy.indexOf(img2Id);
-    
-    imgOrderCopy = atualizaLista(img1Id,index1,img2Id,index2);
+    let idImg1 =  r1String + c1String;
+    let idImg2 = r2String+c2String;
+
+    var index1 = lista.indexOf(idImg1);
+    var index2 = lista.indexOf(idImg2);
+
+
+    searchAndReplace(idImg1,index1,idImg2,index2);
 
     resolvido();
+    
 }
